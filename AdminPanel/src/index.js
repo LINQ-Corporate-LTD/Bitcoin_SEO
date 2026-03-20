4535cb85090a13c208ca279978dab6c6a1085fa6
Benny Scott <benny.scott@iq-hub.com>
2026-03-20 22:40:53 +0530

    fix: revert to PUBLIC_URL=/ and remove basename

--- AdminPanel/src/index.js
+++ AdminPanel/src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <React.Fragment>
-      <BrowserRouter basename="/admin">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.Fragment>
